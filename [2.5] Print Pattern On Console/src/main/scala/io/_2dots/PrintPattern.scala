ca38c10e5871416fa94de9211809c3125f10da26
abhijit <abhijit.yadav@2dots.io>
2019-10-03 22:29:06 +0530

    [2.5] Pattern matching Example

--- [2.5] Print Pattern On Console/src/main/scala/io/_2dots/PrintPattern.scala
+++ [2.5] Print Pattern On Console/src/main/scala/io/_2dots/PrintPattern.scala
@@ -1,18 +1,20 @@
 package io._2dots
 
+import java.io.FileInputStream
+import java.util.Properties
+
+import org.omg.CORBA.Environment
+
 class PrintPattern {
   def trianglePattern(n: Int): Unit = {
+    var space = n - 1
     for (l <- 1 to n) {
-      for (h <- 1 to l) {
-        print(" * ")
+      for (h <- 1 to space) {
+        print(" ")
       }
-      println("")
-    }
-  }
-  def rectanglePattern(n: Int): Unit = {
-    for (l <- 1 to n) {
-      for (h <- 1 to n) {
-        print(" * ")
+      space -= 1
+      for (h <- 1 to l * 2 - 1) {
+        print("*")
       }
       println("")
     }
@@ -22,12 +24,13 @@ class PrintPattern {
 object PrintPattern {
   def main(args: Array[String]): Unit = {
     val p = new PrintPattern()
-    println("\nEnter number of rows and column value : ")
-    val n = scala.io.StdIn.readInt()
+    val input = new FileInputStream("src/main/resources/application.properties")
+    val prop = new Properties()
+    prop.load(input)
 
-    println("\nTriangle Pattern......................")
+    val n = prop.getProperty("pattern.val").toInt
+    println("\n................Pattern..................")
     p.trianglePattern(n)
-    println("\nRectangle Pattern......................")
-    p.rectanglePattern(n)
+    input.close()
   }
 }
