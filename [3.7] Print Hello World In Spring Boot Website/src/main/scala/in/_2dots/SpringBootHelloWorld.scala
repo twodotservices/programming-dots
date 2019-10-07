1ab4a0eb70df55ae5a3465745e3b455aaf2de762
abhijit <abhijit.yadav@2dots.io>
2019-10-07 21:57:53 +0530

    [3.7] Hello World SpringBoot Project

---
+++ [3.7] Print Hello World In Spring Boot Website/src/main/scala/in/_2dots/SpringBootHelloWorld.scala
@@ -0,0 +1,13 @@
+package in._2dots
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringBootHelloWorld {
+}
+object SpringBootHelloWorld {
+  def main(args: Array[String]): Unit = {
+    SpringApplication.run(classOf[SpringBootHelloWorld]);
+  }
+}
