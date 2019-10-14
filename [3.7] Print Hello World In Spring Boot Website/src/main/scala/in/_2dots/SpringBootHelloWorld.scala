94e704f46182dee16252b4ee0a44e8905df9cfa1
abhijit <abhijit.yadav@2dots.io>
2019-10-14 19:57:53 +0530

    [3.7] Hello World SpringBoot Project

--- [3.7] Print Hello World In Spring Boot Website/src/main/scala/in/_2dots/SpringBootHelloWorld.scala
+++ [3.7] Print Hello World In Spring Boot Website/src/main/scala/in/_2dots/SpringBootHelloWorld.scala
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 @SpringBootApplication
 class SpringBootHelloWorld {
 }
+
 object SpringBootHelloWorld {
   def main(args: Array[String]): Unit = {
     SpringApplication.run(classOf[SpringBootHelloWorld]);
--- [3.7] Print Hello World In Spring Boot Website/src/main/scala/in/_2dots/controller/HomeController.scala
+++ [3.7] Print Hello World In Spring Boot Website/src/main/scala/in/_2dots/controller/HomeController.scala
@@ -1,5 +1,4 @@
 package in._2dots.controller
-
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.{GetMapping, RequestMapping, ResponseBody, RestController}
@@ -7,7 +6,7 @@ import org.springframework.web.bind.annotation.{GetMapping, RequestMapping, Resp
 @Controller
 class HomeController {
   @GetMapping(value = Array("/hello"))
-  def helloWorld(model:Model): String ={
+  def helloWorld(model: Model): String = {
     model.addAttribute("message", "Hello World")
     "hello"
   }
